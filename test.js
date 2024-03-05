bd34a742d046f318b858e1880307542dc5956cd2
sharmila657 <sharmilajirel8@gmail.com>
2024-03-05 09:38:13 +0545

    feat:test

---
+++ test.js
@@ -0,0 +1,35 @@
+// YOUR CODE BELOW
+const evenAndOdd =(arr)=>{
+    let evenArr = [];
+    let oddArr = [];
+    for (let i=0; i < arr.length; i++){
+      const currentElement = arr[i];
+      if(currentElement % 2)
+        oddArr.push(currentElement);
+      else evenArr.push(currentElement);
+    }
+    return [evenArr,oddArr]
+  }
+  
+  describe('evenAndOdd', () => {
+  
+    it('is a function', () => {
+      expect(typeof evenAndOdd).toEqual('function');
+    });
+  
+    it('returns an array', () => {
+      let returnedValue = evenAndOdd([1, 2, 3]);
+      expect(Array.isArray(returnedValue)).toEqual(true);
+    });
+  
+    it('returns an array where the first element has all the even numbers from the original array', () => {
+      let returnedValue = evenAndOdd([10, 15, 20, 25, 30]);
+      expect(returnedValue[0]).toEqual([10, 20, 30]);
+    });
+  
+    it('returns an array where the second element has all the odd numbers from the original array', () => {
+      let returnedValue = evenAndOdd([7, 14, 21, 28, 35]);
+      expect(returnedValue[1]).toEqual([7, 21, 35]);
+    });
+  
+  });
